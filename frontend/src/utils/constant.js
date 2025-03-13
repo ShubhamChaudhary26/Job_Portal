b125667695b9e7289ec4c4d236365adb7c871e70
Shubham Chaudhary <sc3617378@gmail.com>
2025-03-13 16:41:40 +0530

    Job Portal Application

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const  USER_API_END_POINT="http://localhost:7777/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:7777/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:7777/api/v1/application";
-export const SINGLECOMPANY_API_END_POINT="http://localhost:7777/api/v1/company";
+export const  USER_API_END_POINT="https://job-portal-b7a0.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-b7a0.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-b7a0.onrender.com/api/v1/application";
+export const SINGLECOMPANY_API_END_POINT="https://job-portal-b7a0.onrender.com/api/v1/company";
